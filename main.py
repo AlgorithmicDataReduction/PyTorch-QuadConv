d69cd4564c783fe199c1dcd87e5370ad75ac4cf9
Cooper <>
2022-06-15 09:31:36 -0600

    Log version is now the experiment name.

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ import yaml
 import torch
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from pytorch_lightning.loggers import TensorBoardLogger
 
 '''
 Build and train a model.
@@ -27,7 +28,7 @@ Input:
     data_args: dataset arguments
     extra_args: other arguments that don't fit in groups above
 '''
-def main(trainer_args, model_args, data_args, extra_args):
+def main(args, trainer_args, model_args, data_args):
     torch.set_default_dtype(torch.float32)
 
     #Setup data
@@ -38,33 +39,34 @@ def main(trainer_args, model_args, data_args, extra_args):
     model = AutoEncoder(**model_args)
 
     #Callbacks
-    callbacks=[ProgressBar()]
-    if extra_args['early_stopping']:
-        callbacks.append(EarlyStopping(monitor="val_loss", patience=3, strict=False))
+    callbacks=[]
+    if args['early_stopping']:
+        callbacks.append(EarlyStopping(monitor="val_loss", patience=5, strict=False))
     if train_args['enable_checkpointing']:
         callbacks.append(ModelCheckpoint(monitor="val_loss", save_last=True, save_top_k=1, mode='min'))
+
+    #Logger
+    if train_args['logger']:
+        train_args['logger'] = TensorBoardLogger(save_dir=train_args['default_root_dir'],
+                                                    version=args['experiment'])
 
     #Train model
     trainer = Trainer(**train_args, callbacks=callbacks)
     trainer.fit(model=model, datamodule=data_module, ckpt_path=None)
 
     #Make GIF
-    if extra_args['make_gif']:
+    if args['make_gif']:
         make_gif(trainer, data_module, None if train_args['enable_checkpointing'] else model)
 
 '''
 Parse arguments
 '''
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--experiment", type=str, default=None)
-    args, _ = parser.parse_known_args()
-
     #Look for CL arguments
+    parser = argparse.ArgumentParser()
     train_parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
     model_parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
     data_parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
-    extra_parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
 
     #trainer args
     train_parser = Trainer.add_argparse_args(train_parser)
@@ -76,29 +78,30 @@ if __name__ == "__main__":
     data_parser = GridDataModule.add_args(data_parser)
 
     #extra args
-    extra_parser.add_argument("--make_gif", type=bool, default=False)
-    extra_parser.add_argument("--early_stopping", type=bool, default=False)
+    parser.add_argument("--experiment", type=str, default=None)
+    parser.add_argument("--make_gif", type=bool, default=False)
+    parser.add_argument("--early_stopping", type=bool, default=False)
 
     #parse remaining args
+    args = vars(parser.parse_known_args()[0])
     train_args = vars(train_parser.parse_known_args()[0])
     model_args = vars(model_parser.parse_known_args()[0])
     data_args = vars(data_parser.parse_known_args()[0])
-    extra_args = vars(extra_parser.parse_known_args()[0])
 
     #Load YAML config
-    if args.experiment != None:
+    if args['experiment'] != None:
         try:
             #open YAML file
-            with open(f"experiments/{args.experiment}.yml", "r") as file:
+            with open(f"experiments/{args['experiment']}.yml", "r") as file:
                 config = yaml.safe_load(file)
 
             #extract args
             train_args.update(config['train'])
             model_args.update(config['model'])
             data_args.update(config['data'])
-            extra_args.update(config['extra'])
+            args.update(config['extra'])
 
         except Exception as e:
-            raise ValueError(f"Experiment {args.experiment} is invalid.")
+            raise ValueError(f"Experiment {args['experiment']} is invalid.")
 
-    main(train_args, model_args, data_args, extra_args)
+    main(args, train_args, model_args, data_args)
--- scripts/blanca_ignition_test.sh
+++ scripts/blanca_ignition_test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#SBATCH --time=06:00:00
+#SBATCH --time=01:00:00
 #SBATCH --qos=preemptable
 #SBATCH --job-name=ignition_test
 #SBATCH --gres=gpu
 #SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
 
 ROOT=/projects/cosi1728/QuadConv
 TEST=blanca_ignition_cnn
+DATA=data/ignition_square/train.npy
 
 module purge
 module load anaconda
@@ -14,7 +16,9 @@ module load anaconda
 conda activate compression
 
 #copy dataset to scratch
-cp $ROOT/data/ignition_square/train.npy $SLURM_SCRATCH/
+cp $ROOT/$DATA $SLURM_SCRATCH/
+
+mkdir $SLURM_SCRATCH/lightning_logs
 
 python $ROOT/main.py --experiment $TEST --default_root_dir $SLURM_SCRATCH --data_dir $SLURM_SCRATCH
 
