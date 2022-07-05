03e188a6f40aa87f8958161538e6eafa4e7bc10d
Cooper <>
2022-07-05 12:56:12 -0600

    Moving a few things around and editing scripts.

--- scripts/blanca_cnn.sh
+++ scripts/blanca_cnn.sh
@@ -2,14 +2,15 @@
 #SBATCH --time=04:00:00
 #SBATCH --qos=preemptable
 #SBATCH --job-name=ignition_cnn
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks-per-node=4
+#SBATCH --no-requeue
 
 #ntasks per node should be num_workers*num_gpus
 
 ROOT=/projects/cosi1728/QuadConv
-TEST=blanca_ignition_cnn_full
+TEST=ignition_cnn_full
 DATA=data/ignition_square/train.npy
 
 module purge
@@ -20,12 +21,7 @@ conda activate compression
 #copy dataset to scratch
 cp $ROOT/$DATA $SLURM_SCRATCH/
 
-mkdir $SLURM_SCRATCH/lightning_logs
-
-python $ROOT/main.py --experiment $TEST --default_root_dir $SLURM_SCRATCH --data_dir $SLURM_SCRATCH
-
 #remove old logs
 rm -r $ROOT/lightning_logs/$TEST
 
-#copy logs from scratch
-cp -r $SLURM_SCRATCH/lightning_logs/$TEST $ROOT/lightning_logs/
+python $ROOT/main.py --experiment $TEST --default_root_dir $ROOT --data_dir $SLURM_SCRATCH
