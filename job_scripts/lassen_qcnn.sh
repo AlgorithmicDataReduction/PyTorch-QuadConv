30767d0d8a96b2b20e2eb56fef5fc546b07842bf
Kevin Doherty <kevindoh14@gmail.com>
2022-10-18 16:43:14 -0600

    job script update

--- job_scripts/lassen_qcnn.sh
+++ job_scripts/lassen_qcnn.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-    ### LSF syntax
-    #BSUB -B
-    #BSUB -N
-    #BSUB -eo mystderr_%J.txt
-    #BSUB -cwd
-    #BSUB -nnodes 2                   #number of nodes
-    #BSUB -W 120                      #walltime in minutes
-    #BSUB -G doherty8                 #account
-    #BSUB -e myerrors.txt             #stderr
-    #BSUB -o myoutput.txt             #stdout
-    #BSUB -J qcnn                    #name of job
-    #BSUB -q pbatch                   #queue to use
+### LSF syntax
+#BSUB -B
+#BSUB -N
+#BSUB -eo mystderr_%J.txt
+#BSUB -cwd
+#BSUB -nnodes 2                   #number of nodes
+#BSUB -W 120                      #walltime in minutes
+#BSUB -G doherty8                 #account
+#BSUB -J qcnn                     #name of job
+#BSUB -q pbatch                   #queue to use
 
     ### Shell scripting
     date; hostname
     echo -n 'JobID is '; echo $LSB_JOBID
 
-    REPO=~/QuadConv
-    SAVE=/usr/workspace/doherty8
-    TEST=ignition_maxskip_qcnn
+    #REPO=~/QuadConv
+    #SAVE=/usr/workspace/doherty8
+    #TEST=ignition_maxskip_qcnn
     DATA=data/ignition_center_cut
 
     module load cuda/11.3.0
@@ -28,9 +26,9 @@
     conda info
 
     #Confirm pytorch detects GPUs
-    python -c "import torch; print(f'GPUs available: {torch.cuda.is_available()}\nNumber of CUDA devices: {torch.cuda.device_count()}\n')"
+    #~/.conda/envs/compression/bin/python -c "import torch; print(f'GPUs available: {torch.cuda.is_available()}\nNumber of CUDA devices: {torch.cuda.device_count()}\n')"
 
     #remove old logs
     #rm -r $REPO/lightning_logs/$TEST/*
 
-    lrun ~/.conda/envs/compression/bin/python $REPO/main.py --experiment $TEST --default_root_dir $SAVE --data_dir $SAVE/$DATA
+    lrun ~/.conda/envs/compression/bin/python ~/QuadConv/main.py --experiment ignition_maxskip_qcnn --default_root_dir ~/QuadConv --data_dir /usr/workspace/doherty8/data/ignition_center_cut
