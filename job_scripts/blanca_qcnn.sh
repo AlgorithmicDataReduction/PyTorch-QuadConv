cc87dd66e5fc1a3422026873c30c5d57373367a8
Cooper <simpson.r.cooper@gmail.com>
2022-12-02 09:12:14 -0700

    Adding a Blanca skip QCAE experiment.

--- job_scripts/blanca_qcnn.sh
+++ job_scripts/blanca_qcnn.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-#SBATCH --time=05:00:00
+#SBATCH --time=08:00:00
 #SBATCH --qos=preemptable
 #SBATCH --job-name=ignition_qcnn
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=8
 #SBATCH --no-requeue
 
 #ntasks per node should be num_workers*num_gpus
 
-ROOT=/projects/cosi1728/QuadConv
-TEST=ignition/qcnn_unstructured_center.yaml
-DATA=data/unstructured_ignition_center_cut
-TIME=00:04:45:00
+TEST=ignition/ignition_skip_qcae.yaml
+TIME=00:07:50:00
 
 module purge
 module load anaconda
 
 conda activate compression
 
-python $ROOT/main.py --experiment $TEST --default_root_dir $ROOT/lightning_logs --data_dir $ROOT/$DATA --max_time $TIME
+python main.py --experiment $TEST --max_time $TIME
