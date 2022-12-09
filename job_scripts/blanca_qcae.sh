ebbdb73d0eda2d764e380d9b7c8bd8d0a7485ea8
Cooper <simpson.r.cooper@gmail.com>
2022-12-09 10:50:47 -0700

    Setting up uniform pool QCAE for Blanca.

--- job_scripts/blanca_qcae.sh
+++ job_scripts/blanca_qcae.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --time=24:00:00
+#SBATCH --time=12:00:00
 #SBATCH --qos=preemptable
 #SBATCH --job-name=ignition_qcae
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks-per-node=4
 #SBATCH --no-requeue
 
 #ntasks per node should be num_workers*num_gpus
 
-TEST=ignition/skip_qcae.yaml
+TEST=ignition/pool_qcae_uniform.yaml
 TIME=00:23:50:00
 
 module purge
