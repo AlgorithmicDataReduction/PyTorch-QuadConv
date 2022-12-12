1bdd92108bea7cbefbab5fb7f811bd5ea5fb076e
Cooper <simpson.r.cooper@gmail.com>
2022-12-12 10:04:53 -0700

    Fixing one bug in the torch_quadconv init with removal of point handler.

--- core/torch_quadconv/__init__.py
+++ core/torch_quadconv/__init__.py
@@ -3,5 +3,4 @@ Import classes that should be immediately available to the user.
 '''
 from .quadconv import QuadConv
 from .mesh_handler import MeshHandler
-from .point_cloud_handler import PointCloudHandler
 from .mesh_datamodule import MeshDataModule
