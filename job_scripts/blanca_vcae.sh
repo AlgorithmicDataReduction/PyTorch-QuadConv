d935787e60a4d8b3afce91b08957e3c62ae0b4e5
Cooper <simpson.r.cooper@gmail.com>
2022-12-20 11:57:06 -0500

    Adding job scripts and experiment for VCAE.

---
+++ job_scripts/blanca_vcae.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --time=12:00:00
+#SBATCH --qos=preemptable
+#SBATCH --job-name=ignition_vcae
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --no-requeue
+
+#ntasks per node should be num_workers*num_gpus
+
+TEST=ignition/vcae_pool.yaml
+TIME=00:11:50:00
+
+module purge
+module load anaconda
+
+conda activate compression
+
+python main.py --experiment $TEST --max_time $TIME
