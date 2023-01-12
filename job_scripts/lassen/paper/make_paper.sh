870d2b672b2e5c3bd4c9c13b1cf4d9ea21b92620
Kevin Doherty <kevindoh14@gmail.com>
2023-01-12 11:14:58 -0700

    typo

--- job_scripts/lassen/paper/make_paper.sh
+++ job_scripts/lassen/paper/make_paper.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-bsub ~/QuadConv/job_scripts/lassen/paper_test1
-bsub ~/QuadConv/job_scripts/lassen/paper_test2
-bsub ~/QuadConv/job_scripts/lassen/paper_test3
-bsub ~/QuadConv/job_scripts/lassen/paper_test4
-bsub ~/QuadConv/job_scripts/lassen/paper_test5
-bsub ~/QuadConv/job_scripts/lassen/paper_test6
-bsub ~/QuadConv/job_scripts/lassen/paper_test7
-bsub ~/QuadConv/job_scripts/lassen/paper_test8
+bsub ~/QuadConv/job_scripts/lassen/paper/paper_test1
+bsub ~/QuadConv/job_scripts/lassen/paper/paper_test2
+bsub ~/QuadConv/job_scripts/lassen/paper/paper_test3
+bsub ~/QuadConv/job_scripts/lassen/paper/paper_test4
+bsub ~/QuadConv/job_scripts/lassen/paper/paper_test5
+bsub ~/QuadConv/job_scripts/lassen/paper/paper_test6
+bsub ~/QuadConv/job_scripts/lassen/paper/paper_test7
+bsub ~/QuadConv/job_scripts/lassen/paper/paper_test8
 
 
