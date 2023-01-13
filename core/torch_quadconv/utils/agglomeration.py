2f1c9f8966a042e5155ed6a68c0ab04c84c17d7b
Cooper <simpson.r.cooper@gmail.com>
2023-01-13 11:33:08 -0700

    Building test dataset based on the ignition mesh.

--- core/torch_quadconv/utils/agglomeration.py
+++ core/torch_quadconv/utils/agglomeration.py
@@ -19,6 +19,24 @@ Input:
 
 NOTE: Compile the .so file using the following command
     gcc -shared -o <output_name>.so -fPIC <name>.c
+
+/*
+NOTE: All multi-dimensional arrays are falttened C-style, i.e. row-wise.
+
+Input:
+  activity: bool* pointer of size (num_points X stages), active points at each stage
+  points: double* pointer of size (num_points X spatial_dim), mesh points
+  adj_indices: int* pointer of size (num_points+1), METIS xadj
+  adjacency: int* pointer of size (2*(number of edges)), METIS adjncy
+  boundary: int* pointer of size (num_boundary_points), indicies of boundary points in points array
+  spatial_dim: int, spatial dimension of mesh points
+  num_points: int, number of mesh points
+  num_boundary_points: int, number of boundary points
+  stages: int, number of coarsening stages
+  factor: int, agglomerate division factor
+*/
+void agglomerate(activity, points, adj_indices, adjacency, boundary,
+                  int spatial_dim, int num_points, int num_boundary_points, int stages, int factor)
 '''
 def agglomerate(input_points, adjacency, num_output_points):
 
