315fdcd9b1f829c2a4ca1ec69d8011b42fbe1973
Cooper <>
2023-03-20 13:32:13 -0600

    Removing export of MeshDataModule to avoid dependence on Lightning.

--- torch_quadconv/__init__.py
+++ torch_quadconv/__init__.py
@@ -3,4 +3,3 @@ Import classes that should be immediately available to the user.
 '''
 from .quadconv import QuadConv
 from .mesh_handler import MeshHandler
-from .mesh_datamodule import MeshDataModule
